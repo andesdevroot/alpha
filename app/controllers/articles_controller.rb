9183493e1ae83b494635dcbe910cd37dc02dfa96
andesdevroot <andes.dev.root@gmail.com>
2021-01-24 22:01:47 -0300

    se instala gema bootstrap + se ajustan formularios y controlador de articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
     def index
         @articles = Article.all
     end
@@ -17,7 +19,13 @@ class ArticlesController < ApplicationController
           render 'new'
         end
     end
-  
+    
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      flash[:notice] = "Article was deleted"
+      redirect_to articles_path
+    end
 
    
 
@@ -42,6 +50,10 @@ class ArticlesController < ApplicationController
       private
       def article_params
           params.require(:article).permit(:title, :description)
+      end
+
+      def set_article
+        @article = Article.find(params[:id])
       end
 
 end
