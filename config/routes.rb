07f7d346ae1f6222efa8652d0a5bcfc7c84d3e97
andesdevroot <andes.dev.root@gmail.com>
2021-01-26 19:50:47 -0300

    se actualiza controlador user + vista signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :articles
+  resources :users, except: [:new]
   root 'pages#index'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
 end
+
+
+
 
 
